6c47fc9f6aafa92c645865629aa89f3f4af8b8b6
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-04-29 11:00:29 +0600

    initial commit

---
+++ database-json.js
@@ -0,0 +1,33 @@
+const data = [   //array
+    {    //array[0]
+    "_id": {
+      "$oid": "5968dd23fc13ae04d9000001"
+    },
+    "product_name": "sildenafil citrate",
+    "supplier": "Wisozk Inc",
+    "quantity": 261,
+    "unit_cost": "$10.47"
+    }, 
+    {    //array[1]
+    "_id": {
+      "$oid": "5968dd23fc13ae04d9000002"
+    },
+    "product_name": "Mountain Juniperus ashei",
+    "supplier": "Keebler-Hilpert",
+    "quantity": 292,
+    "unit_cost": "$8.74"
+    }, 
+    {    //array[2]
+    "_id": {
+      "$oid": "5968dd23fc13ae04d9000003"
+    },
+    "product_name": "Dextromathorphan HBr",
+    "supplier": "Schmitt-Weissnat",
+    "quantity": 211,
+    "unit_cost": "$20.53"
+    }
+]
+
+console.log(data[0]._id.$oid);
+console.log(data[1].quantity);
+console.log(data[2].supplier);
